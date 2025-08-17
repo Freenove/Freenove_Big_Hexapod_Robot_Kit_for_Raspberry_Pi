2ab532aa3808ac973fe09a432a198dc3a9ec9975
Orlando Gordillo <ogordillo@miners.utep.edu>
2025-08-16 20:37:23 -0500

    big restructure to accomodate multiple docker containers on diff platforms

---
+++ src/windows_control/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+package_name = 'windows_control'
+
+setup(
+    name=package_name,
+    version='0.0.1',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages', 
+         ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='Orlando E. gordillo',
+    maintainer_email='ogordillo@miners.utep.edu',
+    description='ROS2 node to control the Hexpod robot via windows',
+    license='Apache License 2.0',
+    tests_require=['pytest'],
+    # entry_points={
+    #     'console_scripts': [
+    #         'servo_node = robot_control.servo_node:main',
+    #     ],
+    },
+)
