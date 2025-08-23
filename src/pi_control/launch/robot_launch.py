a8b187fdea3fc56863a41f5774144fb93d9be839
Capt.Computer <ogordillo@miners.utep.edu>
2025-08-23 17:28:15 +0100

    Working version of pi container /w realsense & customo node
    re-wrote the Dockerfile.pi to pull ubuntu:24.04. I set tags for the realsense stuff so that this continues to work in the future
    for other ppl. There is still more forward work to make sure all the libs and utils get pinned as well.
    Had issues with instructions from realsense-ros, ended up having to make librealsense from scratch and installing realsense2-camera/description before using rosdep.

---
+++ src/pi_control/launch/robot_launch.py
@@ -0,0 +1,27 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from ament_index_python.packages import get_package_share_directory
+import os
+
+def generate_launch_description():
+    realsense_launch_path = os.path.join(
+        get_package_share_directory('realsense2_camera'),
+        'launch',
+        'rs_launch.py'
+    )
+
+    return LaunchDescription([
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(realsense_launch_path),
+            launch_arguments={'align_depth.enable': 'true'}.items(),
+        ),
+
+        Node(
+            package='pi_control',
+            executable='servo_node',
+            name='servo_node',
+            output='screen'
+        ),
+    ])
--- src/pi_control/setup.py
+++ src/pi_control/setup.py
@@ -1,4 +1,6 @@
+from glob import glob
 from setuptools import setup
+import os
 
 package_name = 'pi_control'
 
@@ -10,6 +12,8 @@ setup(
         ('share/ament_index/resource_index/packages', 
          ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*launch.[pxy][yma]*')))
+
     ],
     install_requires=['setuptools'],
     zip_safe=True,
